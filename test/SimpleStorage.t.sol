5fa9a81a221cee33d874e3f034ef31adfb44c5ab
Joe Marron <marronjo@tcd.ie>
2025-02-28 11:36:00 +0900

    add simple storage tests

---
+++ test/SimpleStorage.t.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "forge-std/Test.sol";
+
+import {FHE, inEuint8, euint8} from "@fhenixprotocol/contracts/FHE.sol";
+import {Permissioned, Permission} from "@fhenixprotocol/contracts/access/Permissioned.sol";
+
+import {FheEnabled} from "./utils/FheHelper.sol";
+import {Permission, PermissionHelper} from "./utils/PermissionHelper.sol";
+
+import {SimpleStorage} from "../contracts/SimpleStorage.sol";
+
+contract SimpleStorageTest is Test, FheEnabled {
+    address public user;
+    uint256 public userPrivateKey;
+    PermissionHelper private simpleStoragePermissionHelper;
+    Permission private simpleStoragePermission;
+
+    SimpleStorage private simpleStorage;
+
+    function setUp() public {
+        initializeFhe();
+
+        userPrivateKey = 0xA11CE;
+        user = vm.addr(userPrivateKey);
+
+        simpleStorage = new SimpleStorage();
+
+        simpleStoragePermissionHelper = new PermissionHelper(address(simpleStorage));
+        simpleStoragePermission = simpleStoragePermissionHelper.generatePermission(userPrivateKey);
+    }
+
+    function testSetEncryptedNumber() public {
+        uint8 input = 45;
+        inEuint8 memory eInput = encrypt8(input);
+
+        simpleStorage.setEncryptedNumber(eInput);
+
+        string memory encryptedNumber = simpleStorage.getEncryptedNumber(simpleStoragePermission);
+        uint256 output = unseal(address(simpleStorage), encryptedNumber);
+
+        assertEq(input, uint8(output));
+    }
+}
